ec41b06bfa89f994ee241bb57455974597be17c3
fancy.yuan <yuansmin@sina.com>
2018-02-03 17:21:25 +0800

    refine structure

--- api/__init__.py
+++ api/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
-from api.blog import views
-from api.user import views
+from api.blog import blog_api
+from api.user import user_api

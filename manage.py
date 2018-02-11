bf65f3fd32b5e976f750f5b96ee9186bd8c6ae3f
fancy.yuan <yuansmin@sina.com>
2018-02-11 10:08:38 +0800

    add create_superuser cli

--- manage.py
+++ manage.py
@@ -15,5 +15,27 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.command
+def create_superuser():
+    'create a superuser from cmd'   # help text
+
+    import getpass
+
+    email = raw_input('Email: ')
+    for i in range(3):
+        password = getpass.getpass('Password: ')
+        password_1 = getpass.getpass('Retype Password: ')
+        if password == password_1:
+            break
+        print 'Sorry, passwords do not match.'
+    from api.user.models import User
+    user = User(email=email, password=password)
+    user.active = True
+    user.is_admin = True
+    db.session.add(user)
+    db.session.commit()
+    print 'Create superuser success.\n'
+
+
 if __name__ == '__main__':
     manager.run()
